b6a7ffe9c6c42c5cb18dd420ca7231fb3ad5122b
DESKTOP-DIIFD9D\mitch <mitchell.klingler@gmail.com>
2018-08-13 19:46:48 -0300

    teste

---
+++ KasviApp/KasviApp/Views/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KasviApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+            this.InitializeComponent();
+            this.BindingContext = this;
+            this.IsBusy = false;
+
+            //this.BtnLogin.Clicked += BtnLogin_Clicked;
+
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            this.IsBusy = true;
+        }
+    }
+}
