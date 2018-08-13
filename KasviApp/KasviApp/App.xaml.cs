a01b47d89f298e8d6bfa42ad239fe2387677bd76
DESKTOP-DIIFD9D\mitch <mitchell.klingler@gmail.com>
2018-08-13 18:46:45 -0300

    primeiro commit

---
+++ KasviApp/KasviApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using KasviApp.Views;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace KasviApp
+{
+	public partial class App : Application
+	{
+		
+		public App ()
+		{
+			InitializeComponent();
+            MainPage = new Login();
+
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ KasviApp/KasviApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using KasviApp.Models;
+
+namespace KasviApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
